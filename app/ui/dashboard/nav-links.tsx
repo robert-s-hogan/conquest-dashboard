696dfb3cedbfa636a110deab11b4e866e2846ad1
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-11-15 12:24:05 -0800

    fixed usePathname issue.

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import {
   UserGroupIcon,
   HomeIcon,
@@ -7,6 +8,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
 import clsx from "clsx";
 
 const links = [
@@ -30,6 +32,7 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
+            passHref
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
